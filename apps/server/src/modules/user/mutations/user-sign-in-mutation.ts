3e5a8a4f7ac1a253c658cabe7c2b8b2355f499dc
Gustavo Dantas <gusdantas15@gmail.com>
2025-05-06 22:25:00 -0300

    feat(sign-in): add user-sign-in on the server

---
+++ apps/server/src/modules/user/mutations/user-sign-in-mutation.ts
@@ -0,0 +1,74 @@
+import { Types } from 'mongoose';
+import { GraphQLNonNull, GraphQLString } from 'graphql';
+import { successField } from '@entria/graphql-mongo-helpers';
+import { mutationWithClientMutationId } from 'graphql-relay';
+
+import { UserType } from '../user-type';
+import { UserModel } from '../user-model';
+import { UserLoader } from '../user-loader';
+import { GraphQLContext } from '../../../graphql/context';
+import { fieldError } from '@/utils/field-error';
+import { generateToken } from '@/auth';
+import { FieldErrorField } from '@/modules/field-error/field-error-field';
+
+type UserSignInMutationArgs = {
+  email: string;
+  password: string;
+};
+
+const UserSignInMutation = mutationWithClientMutationId({
+  name: 'UserSignInMutation',
+  inputFields: {
+    email: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    password: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  mutateAndGetPayload: async (
+    args: UserSignInMutationArgs,
+    { ctx }: GraphQLContext
+  ) => {
+    const { email, password } = {
+      password: args.password.trim(),
+      email: args.email.trim().toLowerCase(),
+    };
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return { ...fieldError('email', 'User not found'), success: false };
+    }
+
+    const isPasswordCorrect = user.authenticate(password);
+
+    if (!isPasswordCorrect) {
+      return { ...fieldError('password', 'Wrong password'), success: false };
+    }
+
+    const token = generateToken(user);
+
+    return {
+      id: user._id,
+      token,
+      success: 'Sign In successful',
+    };
+  },
+  outputFields: {
+    me: {
+      type: UserType,
+      resolve: async ({ id }, _, context) => {
+        return UserLoader.load(context, id as Types.ObjectId);
+      },
+    },
+    token: {
+      type: GraphQLString,
+      resolve: ({ token }) => token,
+    },
+    ...FieldErrorField,
+    ...successField,
+  },
+});
+
+export { UserSignInMutation };
--- apps/server/src/schema/mutation-type.ts
+++ apps/server/src/schema/mutation-type.ts
@@ -1,9 +1,11 @@
 import { GraphQLObjectType } from 'graphql';
 import { UserSignUpMutation } from '@/modules/user/mutations/user-sign-up-mutation';
+import { UserSignInMutation } from '@/modules/user/mutations/user-sign-in-mutation';
 
 export const MutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
     userSignUp: UserSignUpMutation,
+    userSignIn: UserSignInMutation,
   }),
 });
