8887c4b4155181bee4f36355d807478eaf65f27e
Gustavo Dantas <gusdantas15@gmail.com>
2025-05-10 14:31:00 -0300

    chore(relay): configure vite to use relay

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,12 +3,37 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import graphql from '@rollup/plugin-graphql';
+import relay from 'vite-plugin-relay';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [graphql(), tailwindcss(), reactRouter(), tsconfigPaths(), relay],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app'),
     },
+  },
+  ssr: {
+    noExternal: [/^relay-runtime(?:\/|$)/, /react-relay(?:\/|$)/],
+    optimizeDeps: {
+      include: [
+        'relay-runtime',
+        'relay-runtime/experimental',
+        'react-relay',
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+      ],
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'relay-runtime',
+      'relay-runtime/experimental',
+      'react-relay',
+      'react',
+      'react-dom',
+      'react/jsx-runtime',
+    ],
   },
 });
