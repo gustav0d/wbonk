4d27caee6732a0031098bc398ed11224ae7b6746
Gustavo Dantas <gusdantas15@gmail.com>
2025-05-10 14:26:11 -0300

    chore(relay): add network

---
+++ apps/web/app/relay/network.ts
@@ -0,0 +1,101 @@
+import {
+  type CacheConfig,
+  type ConcreteRequest,
+  type RequestParameters,
+  type Variables,
+  Network,
+  QueryResponseCache,
+} from 'relay-runtime';
+
+const ONE_MINUTE_IN_MS = 60 * 1000;
+
+function createNetwork() {
+  const responseCache = new QueryResponseCache({
+    size: 100,
+    ttl: ONE_MINUTE_IN_MS,
+  });
+
+  async function fetchResponse(
+    operation: RequestParameters,
+    variables: Variables,
+    cacheConfig: CacheConfig
+  ) {
+    const { id } = operation;
+
+    const isQuery = operation.operationKind === 'query';
+    const forceFetch = cacheConfig && cacheConfig.force;
+
+    if (isQuery && id && !forceFetch) {
+      const fromCache = responseCache.get(id, variables);
+      if (fromCache != null) {
+        return Promise.resolve(fromCache);
+      }
+    }
+
+    return networkFetch(operation, variables);
+  }
+
+  const network = Network.create(fetchResponse);
+  // @ts-ignore Private API Hackery? 🤷‍♂️
+  network.responseCache = responseCache;
+  return network;
+}
+/**
+ * Relay requires developers to configure a "fetch" function that tells Relay how to load
+ * the results of GraphQL queries from your server (or other data source). See more at
+ * https://relay.dev/docs/en/quick-start-guide#relay-environment.
+ */
+
+// dev: http://127.0.0.1:3001/graphql
+const GRAPHQL_ENPOINT = import.meta.env.VITE_GRAPHQL_API_ENDPOINT as string;
+
+async function networkFetch(
+  params: RequestParameters,
+  variables: Variables,
+  headers?: HeadersInit
+) {
+  const response = await fetch(GRAPHQL_ENPOINT, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify({
+      query: params.text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  const json = await response.json();
+
+  // GraphQL returns exceptions (for example, a missing required variable) in the "errors"
+  // property of the response. If any exceptions occurred when processing the request,
+  // throw an error to indicate to the developer what went wrong.
+  if (Array.isArray(json.errors)) {
+    throw new Error(
+      `Error fetching GraphQL query '${params.name}' with variables '${JSON.stringify(variables)}': ${JSON.stringify(
+        json.errors
+      )}`
+    );
+  }
+
+  // Otherwise, return the full payload.
+  return json;
+}
+
+async function getPreloadedQuery(
+  { params }: ConcreteRequest,
+  variables: Variables,
+  headers?: HeadersInit
+) {
+  const response = await networkFetch(params, variables, headers);
+  return {
+    params,
+    variables,
+    response,
+  };
+}
+
+export { createNetwork, getPreloadedQuery };
